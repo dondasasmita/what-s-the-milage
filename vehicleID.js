9ff7921dc37f028c980ba9b8c8b83d3a1cfa0bdb
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-11-29 18:09:12 +0800

    fixed return vehicle ID using callback

--- vehicleID.js
+++ vehicleID.js
@@ -3,10 +3,11 @@
  * NOTE:
  */
 
+// Load all dependencies
 const request = require("request");
 const { email, password, token, server } = require("./config");
 
-// To create callback
+// Function to get vehicle ID from vehicle number given
 const getVehicleID = (vehicleNumber, callback) => {
   request(
     {
@@ -14,26 +15,31 @@ const getVehicleID = (vehicleNumber, callback) => {
       json: true
     },
     (err, response, body) => {
-      if (err) {
-        console.log(`Err: ${err}`);
-        console.log(response.statusCode);
-      } else {
-        console.log(response.statusCode);
-        let data = body;
-        for (let i = 0; i < data.length; i++) {
-          if (data[i].name.toString() === vehicleNumber) {
-            let id = data[i].id;
-            console.log(id);
+      if (!err && response.statusCode == 200) {
+        let id;
+        for (let i = 0; i < body.length; i++) {
+          if (body[i].name === vehicleNumber) {
+            id = body[i].id;
           }
         }
+        callback(undefined, id);
+      } else {
+        // to handle the error
+        callback("Unable to find vehicle ID");
       }
     }
   ).auth(email, password);
 };
 
-// module.exports = {
-//   getVehicleID
-// };
+module.exports = {
+  getVehicleID
+};
 
 // TEST RUN THE FUNCTION
-console.log(getVehicleID("SKAxxxxc"));
+// getVehicleID("SLHxxxZ", (err, result) => {
+//   if (!err) {
+//     console.log(result);
+//   } else {
+//     console.log(err);
+//   }
+// });
